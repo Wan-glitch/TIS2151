7cc047bf6d0573a56b2e44b0e3f4364e5328e74d
JynTong <101113518+JynTong@users.noreply.github.com>
2022-03-31 13:02:56 +0800

    Update title_index.php

--- title_index.php
+++ title_index.php
@@ -14,7 +14,7 @@
 						<div class="span8">
 						
 								<div class="title">
-							<p class="chmsc">Carlos Hilado Memorial State College - Talisay</p>
+							<p class="chmsc">Multimedia University</p>
 							<h3>
 
 							<p>M - Learning</p>
@@ -30,7 +30,7 @@
 						<div class="span12">
 						<br>
 								<div class="motto">
-												<p>CHMSC EXCELS:</p>
+												<p>MMU EXCELS:</p>
 												<p>Excellence, Competence and Educational</p>
 												<p>Leadership in Science and Technology</p>
 								</div>		
